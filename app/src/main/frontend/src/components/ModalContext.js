69085925e759f42e19f59bddf4c2ce3516ec8b47
backnback <garam8796@naver.com>
2024-11-13 12:59:48 +0900

    제거 전

--- app/src/main/frontend/src/components/ModalContext.js
+++
@@ -1,9 +0,0 @@
-import { createContext, useContext, useEffect, useState } from 'react';
-
-
-export const ModalsDispatchContext = createContext({
-    open: () => { },
-    close: () => { },
-});
-
-export const ModalsStateContext = createContext([]);
